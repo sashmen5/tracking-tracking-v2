0b6831ecc92199bc4ec6ee2ea3a6407929b19658
Alexander Mendelevych <sashmen5@gmail.com>
2019-05-14 16:03:12 +0300

    Prettier test

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,22 @@
-import React, {FC, useRef, useState} from 'react';
-import styled from 'styled-components';
+import React, { FC, useRef, useState } from "react";
+import styled from "styled-components";
 // @ts-ignore
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import {Keyed, Project} from '../models';
+import { Keyed, Project } from "models";
 
-import {AppState} from '../store/reducers';
-import {addProject, deleteProject, editProject} from '../store/actions';
+import { AppState } from "../store/reducers";
+import { addProject, deleteProject, editProject } from "../store/actions";
 
-import {Button, Container, SpacedBottomInput, Title} from './CommontStyledComponents';
-import Modal from './Modal';
-import ProjectItem from './ProjectItem';
-import withLoader from '../HOCs/withLoader';
+import {
+  Button,
+  Container,
+  SpacedBottomInput,
+  Title
+} from "./CommontStyledComponents";
+import Modal from "./Modal";
+import ProjectItem from "./ProjectItem";
+import withLoader from "../HOCs/withLoader";
 
 const Wrapper = styled.div`
   margin: 0 auto;
@@ -30,11 +35,11 @@ const ModalContent = styled.div`
 `;
 
 interface ModalWrapperProps {
-    openModal: boolean
+  openModal: boolean;
 }
 
 const ModalWrapper = styled.div<ModalWrapperProps>`
-  visibility: ${props => props.openModal ? '' : 'hidden'};
+  visibility: ${props => (props.openModal ? "" : "hidden")};
 `;
 
 const Label = styled.div`
@@ -43,8 +48,8 @@ const Label = styled.div`
 `;
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const ProjectsButton = styled(Button)`
@@ -55,102 +60,102 @@ const ProjectsButton = styled(Button)`
 const LoadingModalContentWrapper = withLoader(ModalContent);
 
 const Projects: FC = () => {
-    const reduxProjects: Keyed<Project> = useSelector((state: AppState) => state.projects);
-    const dispatch = useDispatch();
+  const reduxProjects: Keyed<Project> = useSelector(
+    (state: AppState) => state.projects
+  );
+  const dispatch = useDispatch();
 
-    const [editProjectId, setEditProjectId] = useState<number | null>(null);
-    const [projectLabel, setProjectLabel] = useState<string>('');
-    const [openModal, setOpenModal] = useState<boolean>(false);
-    const [savingProject, setSavingProject] = useState<boolean>(false);
-    const inputEl = useRef<HTMLInputElement>(null);
+  const [editProjectId, setEditProjectId] = useState<number | null>(null);
+  const [projectLabel, setProjectLabel] = useState<string>("");
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [savingProject, setSavingProject] = useState<boolean>(false);
+  const inputEl = useRef<HTMLInputElement>(null);
 
+  const handleSaveProjectClicked = () => {
+    if (!projectLabel) {
+      return;
+    }
 
-    const handleSaveProjectClicked = () => {
-        if (!projectLabel) {
-            return
-        }
+    setSavingProject(true);
+    if (editProjectId) {
+      dispatch(editProject(editProjectId, projectLabel));
+      setEditProjectId(null);
+    } else {
+      dispatch(addProject(projectLabel));
+    }
 
-        setSavingProject(true);
-        if (editProjectId) {
-            dispatch(editProject(editProjectId, projectLabel));
-            setEditProjectId(null);
-        } else {
-            dispatch(addProject(projectLabel));
-        }
+    //This 'setTimeout' required only to show that loader works. (Mock functionality)
+    setTimeout(() => {
+      setProjectLabel("");
+      setSavingProject(false);
+      setOpenModal(false);
+    }, 100);
+  };
 
+  const handleDeleteProject = (projectId: number) => {
+    dispatch(deleteProject(projectId));
+  };
 
-        //This 'setTimeout' required only to show that loader works. (Mock functionality)
-        setTimeout(() => {
-            setProjectLabel('');
-            setSavingProject(false);
-            setOpenModal(false);
-        }, 100);
-    };
+  const handleEditProject = (id: number) => {
+    const projectLabel: string = reduxProjects[id].label;
+    setProjectLabel(projectLabel);
+    setEditProjectId(id);
+    handleOpenModal();
+  };
 
-    const handleDeleteProject = (projectId: number) => {
-        dispatch(deleteProject(projectId));
-    };
+  const handleOpenModal = () => {
+    setOpenModal(true);
+    requestAnimationFrame(() => {
+      if (inputEl && inputEl.current) {
+        inputEl.current.focus();
+      }
+    });
+  };
 
-    const handleEditProject = (id: number) => {
-        const projectLabel: string = reduxProjects[id].label;
-        setProjectLabel(projectLabel);
-        setEditProjectId(id);
-        handleOpenModal();
-    };
+  const projectKeys: string[] = Object.keys(reduxProjects);
 
-    const handleOpenModal = () => {
-        setOpenModal(true);
-        requestAnimationFrame(() => {
-            if (inputEl && inputEl.current) {
-                inputEl.current.focus()
-            }
-        })
-    };
+  return (
+    <>
+      <Title>Projects</Title>
+      <Wrapper>
+        <ButtonWrapper>
+          <ProjectsButton onClick={e => handleOpenModal()}>
+            Add project
+          </ProjectsButton>
+        </ButtonWrapper>
+        <Container>
+          {projectKeys.length ? (
+            projectKeys.map((key: string) => (
+              <ProjectItem
+                key={`${reduxProjects[key].id}`}
+                project={reduxProjects[key]}
+                handleDeleteProject={handleDeleteProject}
+                handleEditProject={handleEditProject}
+              />
+            ))
+          ) : (
+            <h3>Please add project</h3>
+          )}
+        </Container>
+      </Wrapper>
 
-    const projectKeys: string[] = Object.keys(reduxProjects);
-
-    return (
-        <>
-            <Title>Projects</Title>
-            <Wrapper>
-                <ButtonWrapper>
-                    <ProjectsButton onClick={e => handleOpenModal()}>Add project</ProjectsButton>
-                </ButtonWrapper>
-                <Container>
-                    {
-                        projectKeys.length
-                            ?
-                            projectKeys.map((key: string) =>
-                                <ProjectItem
-                                    key={`${reduxProjects[key].id}`}
-                                    project={reduxProjects[key]}
-                                    handleDeleteProject={handleDeleteProject}
-                                    handleEditProject={handleEditProject}
-                                />
-                            )
-                            :
-                            <h3>Please add project</h3>
-                    }
-                </Container>
-            </Wrapper>
-
-            <ModalWrapper openModal={openModal}>
-                <Modal closeModal={() => setOpenModal(false)}>
-                    <LoadingModalContentWrapper isLoading={savingProject}>
-                        <Label>Add new project</Label>
-                        <span>Project label</span>
-                        <SpacedBottomInput
-                            type='text'
-                            name='projectLabel'
-                            value={projectLabel}
-                            ref={inputEl}
-                            onChange={e => setProjectLabel(e.target.value)}
-                        />
-                        <Button onClick={handleSaveProjectClicked}>Save</Button>
-                    </LoadingModalContentWrapper>
-                </Modal>
-            </ModalWrapper>
-        </>
-    )
+      <ModalWrapper openModal={openModal}>
+        <Modal closeModal={() => setOpenModal(false)}>
+          <LoadingModalContentWrapper isLoading={savingProject}>
+            <Label>Add new project</Label>
+            <span>Project label</span>
+            <SpacedBottomInput
+              type="text"
+              name="projectLabel"
+              value={projectLabel}
+              ref={inputEl}
+              onChange={e => setProjectLabel(e.target.value)}
+            />
+            <Button onClick={handleSaveProjectClicked}>Save</Button>
+          </LoadingModalContentWrapper>
+        </Modal>
+      </ModalWrapper>
+    </>
+  );
 };
 export default Projects;

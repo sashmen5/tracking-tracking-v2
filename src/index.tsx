ba3eebb3b296fdc71609aa8b3cfdd8a915acf86b
Alexander Mendelevych <sashmen5@gmail.com>
2019-05-14 16:10:25 +0300

    Prettier and husky added to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
 
-import store from "./store/rootStore";
+import store from './store/rootStore';
 
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
